5a2782dade1dcb8f2963a64bcaf4f6679cc82f1b
congsole <139826182+congsole@users.noreply.github.com>
2024-03-19 22:28:51 +0900

    Title: 몫 출력하기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240319/몫 출력하기/print-share.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int cnt = 0;
+        while(true) {
+            int a = sc.nextInt();
+            if(a % 2 == 0) {
+                System.out.println(a/2);
+                cnt++;
+                if(cnt ==3 ) {
+                    break;
+                }
+            }
+        }
+    }
+}
