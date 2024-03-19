ccc8b48c40d460378cb6308ca0094239b08ca7e0
congsole <139826182+congsole@users.noreply.github.com>
2024-03-19 21:29:03 +0900

    Title: 1까지 나누기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240319/1까지 나누기/divide-by-1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        
+        for(int i = 1; i < 5000; i++) {
+            n =  n/i;
+            cnt++;
+            if(n == 0) {
+                break;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
