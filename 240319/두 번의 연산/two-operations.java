d37c195ca33ab5b1e2736c5d7c9390a92fd9017f
congsole <139826182+congsole@users.noreply.github.com>
2024-03-19 19:43:21 +0900

    Title: 두 번의 연산, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240319/두 번의 연산/two-operations.java
+++ 240319/두 번의 연산/two-operations.java
@@ -3,10 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        int result = 0;
+        
         if(a%2 == 1) {
             a = a + 3;
-        } 
+        }
+        int result = a; 
         if(a%3 == 0) {
             result = a/3;
         }
