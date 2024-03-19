0370d09e45ad3c4f801e8b78db4ec3a87debbff9
congsole <139826182+congsole@users.noreply.github.com>
2024-03-19 20:36:12 +0900

    Title: 두 사람, Time: 107ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240319/두 사람/two-person.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age1 = sc.nextInt();
+        String sex1 = sc.next();
+        int age2 = sc.nextInt();
+        String sex2 = sc.next();
+
+        if((age1>=19&&sex1.equals("M"))||
+        (age2>=19&&sex2.equals("M"))) {
+            System.out.print(1);
+        }
+        else {
+            System.out.print(0);
+        }
+    }
+}
