2544a6be289be6297d1465ccd757584fd4c8c761
congsole <139826182+congsole@users.noreply.github.com>
2024-03-19 20:31:19 +0900

    Title: 둘 중 하나의 배수, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240319/둘 중 하나의 배수/multiple-of-either.java
+++ 240319/둘 중 하나의 배수/multiple-of-either.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.print(a%3 ==0 || a%5 == 0 ? 1 :1);
+        System.out.print(a%3 ==0 || a%5 == 0 ? 1 :0);
     }
 }
