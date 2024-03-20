d4fdae14ff5c2b8feb2d845ab7770ea5434fd20a
congsole <139826182+congsole@users.noreply.github.com>
2024-03-20 19:35:53 +0900

    Title: 숫자 차례로 출력하기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240320/숫자 차례로 출력하기/to-output-numerically.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+    public static void printNum(int n) {
+        if(n == 0) return;
+
+        printNum(n - 1);
+        System.out.print(n + " ");
+    }
+    public static void printNumReverse(int n) {
+        if(n == 0) return;
+
+        System.out.print(n + " ");
+        printNumReverse(n - 1);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printNum(n); 
+        System.out.println();
+        printNumReverse(n);
+    }
+}
