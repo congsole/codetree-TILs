42702b85d02a7c3385d394ca5a944bf145ad83af
congsole <139826182+congsole@users.noreply.github.com>
2024-03-20 10:15:16 +0900

    Title: 9-1 정사각형, Time: 107ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240320/9-1 정사각형/9-1-square.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int m = 9;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                 System.out.print(m);
+                 m--;
+                 if(m == 0) {
+                    m = 9;
+                 }
+            }
+            System.out.println();
+        }
+    }
+}
