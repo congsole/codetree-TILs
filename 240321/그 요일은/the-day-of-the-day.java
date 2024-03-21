ab822fda9ad647f3b695adb07465f2fb8ec7445c
congsole <139826182+congsole@users.noreply.github.com>
2024-03-21 13:53:58 +0900

    Title: 그 요일은, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240321/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt(), d1 = sc.nextInt();
+        int m2 = sc.nextInt(), d2 = sc.nextInt();
+        String a = sc.next();
+
+
+        int[] lastDayOf = new int[] {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] yoil = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    
+        int elapsedDays = 0;
+        int month = m1; 
+        int day = d1;
+
+        while(true) {
+            if(month == m2 && day == d2) {
+                break;
+            }
+            
+            day++;
+            elapsedDays++;
+            
+            if(day > lastDayOf[month]) {
+                day = 1;
+                month++;
+            }
+        }
+
+        int result = elapsedDays/7;
+        if(yoil[elapsedDays % 7].equals(a)) {
+            result++;
+        }
+        System.out.println(result);
+    }
+}
