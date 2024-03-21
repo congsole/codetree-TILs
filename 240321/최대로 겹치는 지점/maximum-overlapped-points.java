38c55601502a9fca4ed3fea4b5c69eece500fa92
congsole <139826182+congsole@users.noreply.github.com>
2024-03-21 16:20:09 +0900

    Title: 최대로 겹치는 지점, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240321/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Line {
+    int xi, xj;
+
+    public Line(int xi, int xj) {
+        this.xi = xi;
+        this.xj = xj;
+    }
+}
+
+
+public class Main {
+    
+    public static void block(int[] x, Line line) {
+        for(int i=line.xi; i<=line.xj; i++) {
+            x[i]++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] x = new int[100];
+        for(int i=0; i<100; i++) {
+            x[i] = 0;
+        }
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        Line[] lineArr = new Line[n];
+        for(int i=0; i<n; i++) {
+            lineArr[i] = new Line(sc.nextInt(), sc.nextInt());
+            block(x, lineArr[i]);
+        }
+
+        Arrays.sort(x);
+
+        System.out.print(x[99]);
+
+
+
+    }
+}
