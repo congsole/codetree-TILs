6f6975d7e623c4b0114241afb59cd9acffaeb625
congsole <139826182+congsole@users.noreply.github.com>
2024-03-21 23:32:47 +0900

    Title: 만나는 그 순간, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240321/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] aAt = new int[1001];
+        int[] bAt = new int[1001];
+
+        int aPoint = 0, bPoint = 0, aTime = 0, bTime = 0;
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+
+        char[] aCommand = new char[n];
+        int[] aDeltaT = new int[n];
+        for(int i=0; i<n; i++) {
+            aCommand[i] = sc.next().charAt(0);
+            aDeltaT[i] = sc.nextInt();
+
+            if(aCommand[i] == 'R') {
+                for(int t = aTime; t< aTime + aDeltaT[i]; t++) {
+                    aAt[t] = aPoint++;
+                }
+                aTime += aDeltaT[i];
+            } else {
+                for(int t = aTime; t< aTime + aDeltaT[i]; t++) {
+                    aAt[t] = aPoint--;
+                }
+                aTime += aDeltaT[i];
+            }
+
+        }
+
+        char[] bCommand = new char[m];
+        int[] bDeltaT = new int[m];
+        for(int i=0; i<m; i++) {
+            bCommand[i] = sc.next().charAt(0);
+            bDeltaT[i] = sc.nextInt();
+
+            if(bCommand[i] == 'R') {
+                for(int t = bTime; t< bTime + bDeltaT[i]; t++) {
+                    bAt[t] = bPoint++;
+                }
+                bTime += bDeltaT[i];
+            } else {
+                for(int t = bTime; t< bTime + bDeltaT[i]; t++) {
+                    bAt[t] = bPoint--;
+                }
+                bTime += bDeltaT[i];
+            }
+        }
+        
+        for(int t=1; t<1001; t++) {
+            if(aAt[t] == bAt[t]) {
+                System.out.print(t);
+                break;
+            }
+        }
+
+    }
+}
