b1395fb731d11aed8b2e96bdbdae824493011a34
congsole <139826182+congsole@users.noreply.github.com>
2024-03-22 13:09:17 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 253ms, Memory: 12MB, Status: Passed - CodeTree

--- 240322/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
+++ 240322/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -2,39 +2,40 @@ import java.util.*;
 
 public class Main {
     
-    public static int n;
-    public static int m;
+    public static int N;
+    public static int M;
 
-    public static boolean inRange(int x, int y) {
-        return x>=0 && x<n && y>=0 && y<m;
+    public static boolean inRange(int n, int m) {
+        return n>=0 && n<N && m>=0 && m<M;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        m = sc.nextInt();
-        n = sc.nextInt();
+        N = sc.nextInt();
+        M = sc.nextInt();
    
-        int[][] matrix = new int[m][n];
+        int[][] matrix = new int[N][M];
 
-        int[] dy = new int[] {0, 1, 0, -1}; //행
-        int[] dx = new int[] {1, 0, -1, 0}; //열
+        int[] dn = new int[] {0, 1, 0, -1}; //행
+        int[] dm = new int[] {1, 0, -1, 0}; //열
     
         int dirNum = 0;
-        int x = 0;
-        int y = 0;
+        int n = 0;
+        int m = 0;
         
-        for(int i = 1; i <= m*n; i++) {
-            matrix[y][x] = i;
+        for(int i = 1; i <= N*M; i++) {
+            matrix[n][m] = i;
 
-            if(!inRange(x + dx[dirNum], y + dy[dirNum]) || matrix[y + dy[dirNum]][x + dx[dirNum]] != 0) {
+            if(!inRange(n + dn[dirNum], m + dm[dirNum]) || matrix[n + dn[dirNum]][m + dm[dirNum]] != 0) {
                 dirNum = (dirNum + 1) % 4;
             }
-            x = x + dx[dirNum];
-            y = y + dy[dirNum];    
+            n = n + dn[dirNum];
+            m = m + dm[dirNum];    
+
         }
 
-        for(int i = 0; i < n; i++ ) {
-            for(int j = 0; j < m; j++) {
+        for(int i = 0; i < N; i++ ) {
+            for(int j = 0; j < M; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
