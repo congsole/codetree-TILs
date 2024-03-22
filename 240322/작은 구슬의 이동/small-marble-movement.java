69b0035af7169c47f5e23a3ac91196e975b6d063
congsole <139826182+congsole@users.noreply.github.com>
2024-03-22 12:18:42 +0900

    Title: 작은 구슬의 이동, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240322/작은 구슬의 이동/small-marble-movement.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static int n;
+
+    public static boolean inRange(int x, int y) {
+        return x >= 1 && x <= n && y >= 1 && y <= n;
+    }
+
+    public static int getDirNum(char dirCmd) {
+        int dirNum = 0;
+        if(dirCmd == 'R') dirNum = 1;
+        if(dirCmd == 'U') dirNum = 0;
+        if(dirCmd == 'D') dirNum = 3;
+        if(dirCmd == 'L') dirNum = 2; 
+        return dirNum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int t = sc.nextInt();
+
+        int y = sc.nextInt();
+        int x = sc.nextInt();
+        int dirNum = getDirNum(sc.next().charAt(0));
+
+        int[] dy = new int[] {1, 0, 0, -1}; // 행
+        int[] dx = new int[] {0, 1, -1, 0}; // 렬
+
+        while(t-- > 0) {
+            y += dy[dirNum];
+            x += dx[dirNum];
+
+            if(!inRange(x,y)) {
+                y -= dy[dirNum];
+                x -= dx[dirNum];
+                dirNum = 3 - dirNum;
+            } 
+        }
+
+        System.out.printf("%d %d", y, x);
+
+    }
+}
