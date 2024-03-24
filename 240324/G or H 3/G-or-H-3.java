8642791c25e0d01456778231f238ae9bae224e26
congsole <139826182+congsole@users.noreply.github.com>
2024-03-24 23:50:10 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240324/G or H 3/G-or-H-3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[100];
+
+        for(int i=0; i<n; i++) {
+            arr[sc.nextInt()] = sc.next().equals("G")? 1 : 2;
+        }
+
+        int maxSum = 0;
+
+        for(int i=0; i<100-k; i++) {
+            int sum = 0;
+            for(int j=i; j<=i+k; j++) {
+                sum += arr[j];
+            }
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        System.out.print(maxSum);
+
+    }
+}
