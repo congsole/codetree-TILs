7ebef07d68377a2662cd233a5b1b38f421589fa0
congsole <139826182+congsole@users.noreply.github.com>
2024-03-24 23:20:15 +0900

    Title: 오목, Time: 190ms, Memory: 10MB, Status: Passed - CodeTree

--- 240324/오목/O-mok.java
+++ 240324/오목/O-mok.java
@@ -3,6 +3,11 @@ import java.util.*;
 public class Main {
 
     public static int MATRIX_SIZE = 19;
+
+    public static boolean inRange(int n, int m) {
+        return n>=0 && n<=MATRIX_SIZE && m>=0 && m<=MATRIX_SIZE;
+    }   
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
@@ -12,7 +17,53 @@ public class Main {
                 plane[i][j] = sc.nextInt();
             }
         }
-        
+        int[] dn = new int[] {0, 1, 1, 1, 0, -1, -1, -1};
+        int[] dm = new int[] {1, 1, 0, -1, -1, -1, 0, 1};
+
+
+        for(int i=1; i<=MATRIX_SIZE; i++) {
+            for(int j=1; j<=MATRIX_SIZE; j++) {
+                if(plane[i][j] == 1 || plane[i][j] == 2) {
+                    int dirNum = 0;
+                    while(dirNum++ <7) {
+                        int count = 1;
+                        int middleN=0, middleM=0;       
+                        while(true) {
+                            if(inRange(i+count*dn[dirNum], j+count*dm[dirNum]) && plane[i+count*dn[dirNum]][j+count*dm[dirNum]] == plane[i][j]) {
+                                count++;
+                                if(count == 2) {
+                                    middleN = i + 2*dn[dirNum];
+                                    middleM = j + 2*dm[dirNum];
+                                }
+                            } else {
+                                break;
+                            }
+                            if(count == 5) {
+                                System.out.println(plane[i][j]);
+                                System.out.printf("%d %d", middleN, middleM);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.print(0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
         int winner = 0;
         int k = 1;
         // k가 이겼는지 찾아보기
@@ -65,6 +116,6 @@ public class Main {
 
         }
         System.out.print(0);
-        
+        */
     }
 }
