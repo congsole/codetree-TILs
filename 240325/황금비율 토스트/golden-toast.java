49dc9f40351ca450d7e8cf8a8233aba73e24606e
congsole <139826182+congsole@users.noreply.github.com>
2024-03-25 12:30:22 +0900

    Title: 황금비율 토스트, Time: 3045ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

---
+++ 240325/황금비율 토스트/golden-toast.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Character> list = new LinkedList<>();
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        String str = sc.next();
+        for(int i=0; i<n; i++) {
+            list.add(str.charAt(i));
+        }
+
+        ListIterator<Character> it = list.listIterator(list.size());
+        for(int i=0; i<m; i++) {
+            char c = sc.next().charAt(0);
+            char cAdd = ' ';
+            if(c == 'P') cAdd = sc.next().charAt(0); 
+            switch(c) {
+                case 'L': if(it.hasPrevious()) it.previous(); break;
+                case 'R': if(it.hasNext()) it.next(); break;
+                case 'D': if(it.hasNext()) {it.next(); it.remove();} break;
+                case 'P': it.add(cAdd); break;
+            }
+        }
+
+        for(int i=0; i<list.size(); i++) {
+            System.out.print(list.get(i));
+        }
+    }
+}
