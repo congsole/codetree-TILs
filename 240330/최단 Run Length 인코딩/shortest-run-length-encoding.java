61f2b06627e61220a059436d93b570880e12d9cd
congsole <139826182+congsole@users.noreply.github.com>
2024-03-30 21:46:08 +0900

    Title: 최단 Run Length 인코딩, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240330/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    
+    public static String runLengthEncoding(char[] arr) {
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for(int i=0;i<arr.length;i++) {
+            if(i > 0 && arr[i] == arr[i-1]) count++;
+            else if(i > 0 && arr[i] != arr[i-1]) {
+                sb.append(arr[i-1]);
+                sb.append(count + "");
+                count = 1;
+            }
+            if(i == arr.length-1) {
+                sb.append(arr[i]);
+                sb.append(count + "");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void rightShift(char[] arr) {
+        char temp = arr[arr.length-1];
+        for(int i=arr.length-1; i>0; i--) {
+            arr[i] = arr[i-1];
+        }
+        arr[0] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] arr = new char[str.length()];
+        for(int i=0;i<arr.length;i++) {
+            arr[i] = str.charAt(i);
+        }
+        int minLength = 1000000000;
+        for(int c=1; c<=arr.length; c++) {
+            rightShift(arr);
+            str = runLengthEncoding(arr);
+            minLength = Math.min(minLength, str.length());
+        }
+
+        System.out.print(minLength);
+    }
+}
