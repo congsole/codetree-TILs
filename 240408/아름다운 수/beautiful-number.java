ff0cd6e653053a72193a991fc57deebb5e678cb5
congsole <139826182+congsole@users.noreply.github.com>
2024-04-08 12:57:37 +0900

    Title: 아름다운 수, Time: 214ms, Memory: 12MB, Status: Passed - CodeTree

--- 240408/아름다운 수/beautiful-number.java
+++ 240408/아름다운 수/beautiful-number.java
@@ -4,29 +4,37 @@ public class Main {
     public static int n, count;
     public static List<Integer> list = new ArrayList<>();
     public static boolean isBeautiful(List<Integer> list) {
-        int cnt = 1;
-        boolean is = true;
-        if(list.size() == 1) {
-            if(list.get(0) != 1) is = false;
-        }
-        for(int i=1; i<list.size(); i++) {
-            if(list.get(i) == list.get(i-1)) {
-                cnt++;
-            } else if(list.get(i) != list.get(i-1)) {
-                if(cnt % list.get(i-1) != 0) {
-                    is = false;
-                    break;
-                }
-                cnt=1;
+        // int cnt = 1;
+        // if(list.size() == 1) {
+        //     if(list.get(0) != 1) return false;
+        // }
+        // for(int i=1; i<list.size(); i++) {
+        //     if(list.get(i) == list.get(i-1)) {
+        //         cnt++;
+        //     } else if(list.get(i) != list.get(i-1)) {
+        //         if(cnt % list.get(i-1) != 0) {
+        //             return false;
+        //         }
+        //         cnt=1;
+        //     }
+        //     if(i == list.size() -1) {
+        //         if(cnt % list.get(i) != 0) {
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+        for(int i=0; i<n; i+=list.get(i)) {
+            if(i+list.get(i)-1 >= n) {
+                return false;
             }
-            if(i == list.size() -1) {
-                if(cnt % list.get(i) != 0) {
-                    is = false;
-                    break;
+            for(int j=i; j<i+list.get(i); j++) {
+                if(list.get(i) != list.get(j)) {
+                    return false;
                 }
             }
         }
-        return is;
+        return true;
     }
     public static void choose(int num) {
         if(num == n+1) {
