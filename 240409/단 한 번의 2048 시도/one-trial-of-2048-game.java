62ee6975c12f37d750cb7db85802068faeb39d34
congsole <139826182+congsole@users.noreply.github.com>
2024-04-09 13:18:03 +0900

    Title: 단 한 번의 2048 시도, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240409/단 한 번의 2048 시도/one-trial-of-2048-game.java
@@ -0,0 +1,116 @@
+import java.util.*;
+
+public class Main {
+    public static int[][] matrix;
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        matrix = new int[4][4];
+
+        for(int i=0; i<4; i++)
+            for(int j=0; j<4; j++)
+                matrix[i][j] = sc.nextInt();
+
+        char cmd = sc.next().charAt(0);
+
+        if(cmd == 'R') {
+            for(int i=0; i<4; i++) {
+                for(int j=4-1; j>0; j--) {
+                    // 댕기기
+                    if(matrix[i][j] == 0) {
+                        matrix[i][j] = matrix[i][j-1];
+                        matrix[i][j-1] = 0;
+                    }
+                }
+                for(int j=3; j>0; j--) {
+                    if(matrix[i][j] == matrix[i][j-1]) {
+                        matrix[i][j] *= 2;
+                        matrix[i][j-1] = 0;
+                        j--;
+                    }
+                }
+                for(int j=4-1; j>0; j--) {
+                    // 댕기기
+                    if(matrix[i][j] == 0) {
+                        matrix[i][j] = matrix[i][j-1];
+                        matrix[i][j-1] = 0;
+                    }
+                }
+            }
+        } else if(cmd == 'L') {
+            for(int i=0; i<4; i++) {
+                for(int j=0; j<3; j++) {
+                    if(matrix[i][j] == 0) {
+                        matrix[i][j] = matrix[i][j+1];
+                        matrix[i][j+1] = 0;
+                    }
+                }
+                for(int j=0; j<3; j++) {
+                    if(matrix[i][j] == matrix[i][j+1]) {
+                        matrix[i][j] *= 2;
+                        matrix[i][j+1] = 0;
+                        j++;
+                    }
+                }
+                for(int j=0; j<3; j++) {
+                    if(matrix[i][j] == 0) {
+                        matrix[i][j] = matrix[i][j+1];
+                        matrix[i][j+1] = 0;
+                    }
+                }
+            }
+        } else if(cmd == 'U') {
+            for(int j=0; j<4; j++) {
+                for(int i=0; i<3; i++) {
+                    if(matrix[i][j] == 0) {
+                        matrix[i][j] = matrix[i+1][j];
+                        matrix[i+1][j] = 0;
+                    }
+                }
+                for(int i=0; i<3; i++) {
+                    if(matrix[i][j] == matrix[i+1][j]) {
+                        matrix[i][j] *= 2;
+                        matrix[i+1][j] = 0;
+                        i++;
+                    }
+                }
+                for(int i=0; i<3; i++) {
+                    if(matrix[i][j] == 0) {
+                        matrix[i][j] = matrix[i+1][j];
+                        matrix[i+1][j] = 0;
+                    }
+                }
+            }
+        } else {
+            for(int j=0; j<4; j++) {
+                for(int i=3; i>0; i--) {
+                    if(matrix[i][j] == 0) {
+                        matrix[i][j] = matrix[i-1][j];
+                        matrix[i-1][j] = 0;
+                    }
+                }
+                for(int i=3; i>0; i--) {
+                    if(matrix[i][j] == matrix[i-1][j]) {
+                        matrix[i][j] *= 2;
+                        matrix[i-1][j] = 0;
+                        i--;
+                    }
+                }
+                for(int i=3; i>0; i--) {
+                    if(matrix[i][j] == 0) {
+                        matrix[i][j] = matrix[i-1][j];
+                        matrix[i-1][j] = 0;
+                    }
+                }
+            }
+        }
+        
+        
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<4; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
