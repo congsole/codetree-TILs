d2e3a974b820e46f673d96a969ce72c138b44916
congsole <139826182+congsole@users.noreply.github.com>
2024-04-10 22:08:23 +0900

    Title: 스승의 은혜 3, Time: 222ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240410/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Gift {
+    int price;
+    int deli;
+    int sum;
+    public Gift(int p, int d) {
+        this.price = p;
+        this.deli = d;
+        this.sum = this.price + this.deli;
+    }
+}
+
+public class Main {
+    public static int N, B;
+    public static List<Gift> list = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        B = sc.nextInt();
+
+        for(int i=0; i<N; i++)
+            list.add(new Gift(sc.nextInt(), sc.nextInt()));
+
+        // Collections.sort(list, (a, b) -> a.sum - b.sum);
+
+        int maxCount = 0;
+        for(int i=0; i<N; i++) {
+            int pay = 0;
+            int count = 0;
+            for(int j=0; j<N; j++) {
+                int initialPay = pay;
+                if(i == j) {
+                    pay = pay + list.get(j).price/2 + list.get(j).deli;
+                } else {
+                    pay = pay + list.get(j).sum;
+                }
+                if(pay <= B) {
+                    count++;
+                } else {
+                    pay = initialPay;
+                }
+            }
+            maxCount = Math.max(maxCount, count);
+        }
+
+        System.out.print(maxCount);
+    }
+}
