fedf81b8b1c8ce349444db0a662eec409ef73a52
congsole <139826182+congsole@users.noreply.github.com>
2024-04-11 22:42:17 +0900

    Title: 최대 최소간의 차, Time: 200ms, Memory: 12MB, Status: Passed - CodeTree

--- 240411/최대 최소간의 차/difference-between-maximum-and-minimum.java
+++ 240411/최대 최소간의 차/difference-between-maximum-and-minimum.java
@@ -12,41 +12,32 @@ public class Main {
         for(int i=0; i<n; i++)
             a[i] = sc.nextInt();
         
-
-            int cost = 0;
-            
-            int[] b = new int[n];
-            for(int i=0; i<n; i++)
-                b[i] = a[i];
-            
-            while(true) {
-                Arrays.sort(b);
-                int min = b[0];
-                int max = b[n-1];
-                if(max-min == k) {
-                    System.out.print(cost);
-                    return;
+        int cost = 0;
+        while(true) {
+            Arrays.sort(a);
+            int min = a[0];
+            int max = a[n-1];
+            if(max-min == k) {
+                System.out.print(cost);
+                return;
+            }
+            int minCount = 0;
+            int maxCount = 0;
+            for(int i=0; i<n; i++) {
+                if(a[i] == a[0]) minCount++;
+                if(a[i] == a[n-1]) maxCount++;
+            }
+            if(minCount>maxCount) {
+                for(int i=n-1; i>n-1-maxCount; i--) {
+                    a[i]--;
+                    cost++;
                 }
-                int minCount = 0;
-                int maxCount = 0;
-                for(int i=0; i<n; i++) {
-                    if(b[i] == b[0]) minCount++;
-                    if(b[i] == b[n-1]) maxCount++;
-                }
-                if(minCount>maxCount) {
-                    for(int i=n-1; i>n-1-maxCount; i--) {
-                        b[i]--;
-                        cost++;
-                    }
-                } else {
-                    for(int i=0; i<minCount; i++) {
-                        b[i]++;
-                        cost++;
-                    }
+            } else {
+                for(int i=0; i<minCount; i++) {
+                    a[i]++;
+                    cost++;
                 }
             }
-
-
-        
+        }    
     }
 }
