515171ad449b6524277b20c34e24c6bcf3a5c4a2
congsole <139826182+congsole@users.noreply.github.com>
2024-04-11 10:07:50 +0900

    Title: 3개의 선 2, Time: 153ms, Memory: 11MB, Status: Passed - CodeTree

--- 240411/3개의 선 2/three-lines-2.java
+++ 240411/3개의 선 2/three-lines-2.java
@@ -1,46 +1,76 @@
 import java.util.*;
 
+class Point {
+    int x, y;
+    boolean cross;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+        boolean cross = false;
+    }
+    public void isCross(char xy, int value) {
+        if(xy == 'x' && this.x == value) {
+            cross = true;
+        } else if(xy == 'y' && this.y == value) {
+            cross = true;
+        }
+    }
+}
 
+class Combination {
+    char i, j, k;
+    public Combination(char i, char j, char k) {
+        this.i = i;
+        this.j = j;
+        this.k = k;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] x = new int[n];
-        int[] y = new int[n];
-
-        int minX = 10, minY = 10;
-        int maxX = 0, maxY = 0;
-        for(int i=0; i<n; i++) {
-            x[i] = sc.nextInt();
-            maxX = Math.max(maxX, x[i]);
-            minX = Math.min(minX, x[i]);
-            y[i] = sc.nextInt();
-            maxY = Math.max(maxY, y[i]);
-            minY = Math.min(minY, y[i]);
-        }
-
-        int xLine[] = new int[11];
-        int yLine[] = new int[11];
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(x[i] == x[j]) xLine[x[i]] = 1;
-            }
-        }
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(y[i] == y[j]) yLine[y[i]] = 1;
-            }
-        }
         
-        int lineCount = 0;
-        for(int i=0; i<11; i++) {
-            lineCount = lineCount + xLine[i] + yLine[i];
+        List<Point> list = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            list.add(new Point(sc.nextInt(), sc.nextInt()));
         }
 
-        if(lineCount < 4) System.out.print(1);
-        else System.out.print(0);
+        int minX = 10000, minY = 10000;
+        int maxX = 0, maxY = 0;
 
+        List<Combination> cList = new ArrayList<>();
+        for(char i='x'; i<='y'; i++) {
+            for(char j='x'; j<='y'; j++) {
+                for(char k='x'; k<='y'; k++) {
+                    cList.add(new Combination(i, j, k));
+                }
+            }
+        }
+        for(Combination c : cList) {
+            for(int q=0; q<=10; q++) {
+                for(int w=0; w<=10; w++) {
+                    for(int e=0; e<=10; e++) {
+                        boolean allCross = true;
+                        for(Point p : list) {
+                            p.cross = false;
+                            p.isCross(c.i, q);
+                            p.isCross(c.j, w);
+                            p.isCross(c.k, e);
+                        }
+                        for(Point p : list) {
+                            if(!p.cross) allCross = false;
+                        }
+                        if(allCross) {
+                            System.out.print(1);
+                            return;
+                        }
+                            
+                    }
+                }
+            }
+        }
+        System.out.print(0);
 
     }
 }
