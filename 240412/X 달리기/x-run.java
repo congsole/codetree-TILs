50d901798e977b8cf3c32ebe22714a219706f16b
congsole <139826182+congsole@users.noreply.github.com>
2024-04-12 13:32:29 +0900

    Title: X 달리기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240412/X 달리기/x-run.java
+++ 240412/X 달리기/x-run.java
@@ -6,37 +6,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         x = sc.nextInt();
 
-        int minT = 1000;
-        for(int vMax=1; vMax<=x; vMax++) {
-            for(int tU=1; tU<=x; tU++) {
-                int t=0;
-                int v=1;
-                int distance=0;
-                int vMaxT = vMax-v;
-                while(true) {
-                    if(t < vMaxT) {
-                        distance += v;
-                        t++;
-                        v++;
-                    } else if(t >= vMaxT && t < vMaxT + tU) {
-                        distance += vMax*tU;
-                        t += tU;
-                        if(v>1) v--;
-                    } else {
-                        distance += v;
-                        t++;
-                        if(v>1) v--;
-                    }
-                    if(distance >= x) {
-                        break;
-                    }
-                }
-                // System.out.println(vMax + " " + tU + " " + t + " " + distance + " " + v);
-                if(distance == x && v == 1) {
-                    minT = Math.min(minT, t);
-                }
-            }   
+        int distance = 0;
+        int t = 0;
+        int v = 1;
+
+        while(t<=x) {
+            distance += v;
+            t++;
+            if(x-distance >= (v+2)*(v+1)/2) {
+                v++;
+            } else if(x-distance >= v*(v+1)/2) {
+                v = v;
+            } else {
+                if(v>1)v--;
+            }
+            // System.out.println(t + " " + v + " " + distance);
+            if(distance == x && v == 1) {
+                System.out.print(t);
+                return;
+            }
         }
-        System.out.print(minT);
+
     }
 }
