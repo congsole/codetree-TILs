d4a583b4f52110249290f283d5afbb97e5ea4f66
congsole <139826182+congsole@users.noreply.github.com>
2024-04-12 15:42:25 +0900

    Title: 독서실의 거리두기 2, Time: 235ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240412/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static char[] a;
+    public static List<Integer> list = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        String str = sc.next();
+        a = str.toCharArray();
+
+        loop1: for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                if(a[i] == '1' && a[j] == '1') {
+                    list.add(j-i);
+                    continue loop1;
+                }
+            }
+        }
+
+        Collections.sort(list, (a,b) -> b - a);
+        System.out.print(Math.min(list.get(0)/2, list.get(list.size()-1)));
+    }
+}
