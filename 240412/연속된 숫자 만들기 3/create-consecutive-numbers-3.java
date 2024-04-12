cbf03c2bfe2b5873fecce493abfdf4eda1d36a93
congsole <139826182+congsole@users.noreply.github.com>
2024-04-12 15:03:45 +0900

    Title: 연속된 숫자 만들기 3, Time: 3085ms, Memory: 11MB, Status: Time Limit Exceed - CodeTree

---
+++ 240412/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static int[] a = new int[3];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<3; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        while(true) {
+            
+            if(a[2] - a[1] <= a[1] - a[0]) {
+                a[2] = a[0] + 1;
+            } else {
+                a[0] = a[2] - 1;
+            }
+
+            count++;
+            Arrays.sort(a);
+            if(a[2] - a[1] == 1 && a[1] - a[0] == 1) {
+                break;
+            }
+        }
+
+        System.out.print(count);
+
+    }
+}
