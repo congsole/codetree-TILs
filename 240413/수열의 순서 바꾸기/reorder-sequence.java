bc63eed53c78572e4f27b72219a433ae6eb243b1
congsole <139826182+congsole@users.noreply.github.com>
2024-04-13 16:16:15 +0900

    Title: 수열의 순서 바꾸기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240413/수열의 순서 바꾸기/reorder-sequence.java
+++ 240413/수열의 순서 바꾸기/reorder-sequence.java
@@ -10,8 +10,10 @@ public class Main {
 
         for(int i=0; i<N; i++) {
             a[i] = sc.nextInt();
-
         }
+        
+        // 특정 인덱스부터 맨 뒤 까지 정렬되어 있다면 그 인덱스의 앞쪽에 있는 수들만 적절한 위치로 옮기면 된다.
+        // 맨 뒤부터 정렬되어있는 수의 갯수를 센다.
         
         int sortedNum = 1;
         for(int i=N-2; i>=0; i--) {
@@ -21,7 +23,7 @@ public class Main {
                 break;
             }
         }
-
+        // 전체 갯수에서 맨뒤 정렬 갯수를 뺀다.
         System.out.print(N-sortedNum);
     }
 }
